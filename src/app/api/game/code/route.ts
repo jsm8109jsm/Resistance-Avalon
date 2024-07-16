930d33f61f7f108d988678db00eba07d5a961402
정 승민 <jsm8109jsm@gmail.com>
2024-07-16 14:49:24 +0000

    FEAT :: 게임 코드 랜덤 생성 api

---
+++ src/app/api/game/code/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const generateRandomString = (num: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+
+export async function GET(req: NextRequest) {
+  const randomCode = generateRandomString(6);
+  return NextResponse.json({
+    code: randomCode,
+  });
+}
